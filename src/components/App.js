6f7495a1f5a9c79629947ea1d1d8a5a835251c10
ArmLil <armlilhov@mail.ru>
2019-02-10 14:19:08 +0300

    try to fix tubs

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,13 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Body from '../components/Body';
 
-
 function App() {
 	return (
     <div>
-  		<Header/>
-  		<Body/>
-  		<Footer/>
-  	</div>
-  )
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>)
 }
 
 export default App;
